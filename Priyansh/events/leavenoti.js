f5c1a44bfe90824fceebaf10ec50e45cb26ca4f8
avcreatx <avcreatx@gmail.com>
2025-01-28 19:18:36 +0530

    Update leavenoti.js

--- Priyansh/events/leavenoti.js
+++ Priyansh/events/leavenoti.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "leave",
 	eventType: ["log:unsubscribe"],
 	version: "1.0.0",
-	credits: "𝘼𝙔𝙐𝙎𝙃 𝙎𝙃𝙐𝙆𝙇𝘼",//Mod by H.Thanh
+	credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
 	description: "Notify the Bot or the person leaving the group with a random gif/photo/video",
 	dependencies: {
 		"fs-extra": "",
@@ -40,10 +40,10 @@ module.exports.run = async function({ api, event, Users, Threads }) {
 
 	if (existsSync(path)) mkdirSync(path, { recursive: true });
 
-(typeof data.customLeave == "undefined") ? msg = "╠💯ChALa gYa ChaNd KaHi aSMaN ko cHoR ke🤧╣\n\n 𝐔𝐬𝐤𝐚 𝐍𝐚𝐚𝐦 » {name} \n\n 𝐑𝐞𝐚𝐬𝐨𝐧 »» {type} \n\n 𝐓𝐢𝐦𝐞 »» {time} \n\n 😍😍 ❣️ {session} " : msg = data.customLeave;
-	msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type).replace(/\{session}/g, hours <= 10 ? "Suprabhat" : 
-    hours > 10 && hours <= 12 ? "Good Afternoon" :
-    hours > 12 && hours <= 18 ? "Good Evening" : "Good Night").replace(/\{time}/g, time);  
+(typeof data.customLeave == "undefined") ? msg = "[⚜️𝙲𝚑𝚊𝚕𝚊 𝚐𝚊𝚢𝚊 𝙲𝚑𝚊𝚗𝚍 𝚔𝚊𝚑𝚊 𝚊𝚊𝚜𝚖𝚊 𝚔𝚘 𝚌𝚑𝚑𝚘𝚛 𝚔𝚎🫂] \n\n 𝐍𝐚𝐦𝐞 : {name} \n 𝐑𝐞𝐚𝐬𝐨𝐧 : {type}  [⚜️]\۩ 🙏🏻\n\n💐<3😊💔 {session} || {time}" : msg = data.customLeave;
+	msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type).replace(/\{session}/g, hours <= 10 ? "𝐆𝐨𝐨𝐝 𝐌𝐨𝐫𝐧𝐢𝐧𝐠" : 
+    hours > 10 && hours <= 12 ? "𝐆𝐨𝐨𝐝 𝐀𝐟𝐭𝐞𝐫𝐧𝐨𝐨𝐧 🌹" :
+    hours > 12 && hours <= 18 ? "𝐆𝐨𝐨𝐝 𝐄𝐯𝐞𝐧𝐢𝐧𝐠" : "𝐆𝐨𝐨𝐝 𝐍𝐢𝐠𝐡𝐭").replace(/\{time}/g, time);  
 
 	const randomPath = readdirSync(join(__dirname, "cache", "leaveGif", "randomgif"));
 
@@ -55,3 +55,4 @@ module.exports.run = async function({ api, event, Users, Threads }) {
 	else formPush = { body: msg }
 	
 	return api.sendMessage(formPush, threadID);
+}
