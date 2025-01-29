f042703946ee95d7a9d506e7d20f55d15acaa54b
avcreatx <avcreatx@gmail.com>
2025-01-29 18:00:31 +0530

    Update mention2.js

--- Priyansh/commands/mention2.js
+++ Priyansh/commands/mention2.js
@@ -10,7 +10,7 @@ module.exports.config = {
 };
 module.exports.handleEvent = function({ api, event }) {
   if (event.senderID !== "44441" {
-    var aid = ["61568085500953" , "61551225242006" , "61557135887844"];
+    var aid = ["61572288249583" , "61551225242006" , "61557135887844"];
     for (const id of aid) {
     if ( Object.keys(event.mentions) == id) {
       var msg = ["Me Jaanu Ke Sath Busy hu , Mujhe kyu bula rahe ho" , "Abe Me bot hu mujhe mention mat kar" , "Dimag Mat khao" , "Kya hua janu mujhe bulaya🙂" , "Koi kaam ni hai kya tujhe" , "Bolo na babu" , "Meri yaad arahi tumhe itni" , "Ha meri jan😙" , "Mujhe mt bulya kro🙄" , "Tera sar f0d dena me , baar mention mt kro😒" , "Ek Bar Me smjh ni ata tujhe kya meri baat ,mujhe baar bar mention krrhe ho😒😒😒😒" , "Abee jaa na 😒" , "Mujhe Bulaya😘kya"];
