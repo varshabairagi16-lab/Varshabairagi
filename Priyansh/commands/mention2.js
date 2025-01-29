98f98d71c87f0fd2e23f48acd78bff2d8d5bf4aa
avcreatx <avcreatx@gmail.com>
2025-01-29 18:16:34 +0530

    Update mention2.js

--- Priyansh/commands/mention2.js
+++ Priyansh/commands/mention2.js
@@ -1,19 +1,19 @@
 module.exports.config = {
-  name: "mention-bot",
+  name: "mention2",
   version: "1.0.0-beta-fixbyDungUwU",
   hasPermssion: 0,
-  credits: "Arun",
+  credits: "ZyrosGenZ-fixbyDungUwU",
   description: "Bot will rep ng tag admin or rep ng tagbot ",
-  commandCategory: "system",
+  commandCategory: "Other",
   usages: "",
   cooldowns: 1
 };
 module.exports.handleEvent = function({ api, event }) {
   if (event.senderID !== "61572288249583") {
-    var aid = ["61572288249583" , "61572288249583" , "61557135887844"];
+    var aid = ["61572288249583" , "61572288249583", "100065518342477","100013065791183" , "100064584243077"];
     for (const id of aid) {
     if ( Object.keys(event.mentions) == id) {
-      var msg = ["Me Jaanu Ke Sath Busy hu , Mujhe kyu bula rahe ho" , "Abe Me bot hu mujhe mention mat kar" , "Dimag Mat khao" , "Kya hua janu mujhe bulaya🙂" , "Koi kaam ni hai kya tujhe" , "Bolo na babu" , "Meri yaad arahi tumhe itni" , "Ha meri jan😙" , "Mujhe mt bulya kro🙄" , "Tera sar f0d dena me , baar mention mt kro😒" , "Ek Bar Me smjh ni ata tujhe kya meri baat ,mujhe baar bar mention krrhe ho😒😒😒😒" , "Abee jaa na 😒" , "Mujhe Bulaya😘kya"];
+      var msg = ["Me Jaanu Ke Sath Busy hu ", "Mujhe kyu bula rahe ho" , "Abe Me bot hu mujhe mention mat kar" , "Dimag Mat khao" , "Kya hua janu mujhe bulaya🙂" , "Koi kaam ni hai kya tujhe" , "Bolo na babu" , "Meri yaad arahi tumhe itni" , "Ha meri jan😙" , "Mujhe mt bulya kro🙄" , "Tera sar f0d dena me , baar mention mt kro😒" , "Ek Bar Me smjh ni ata tujhe kya meri baat ,mujhe baar bar mention krrhe ho😒😒😒😒" , "Abee jaa na 😒" , "Mujhe Bulaya😘kya"];
       return api.sendMessage({body: msg[Math.floor(Math.random()*msg.length)]}, event.threadID, event.messageID);
     }
     }}
