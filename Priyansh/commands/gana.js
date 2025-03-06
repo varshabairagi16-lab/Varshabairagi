0045b7904b3d025018bcb607fa430fc4ee216a70
avcreatx <avcreatx@gmail.com>
2025-03-06 12:50:07 +0530

    Update gana.js

--- Priyansh/commands/gana.js
+++ Priyansh/commands/gana.js
@@ -1,4 +1,4 @@
-module.exports.config = {
+umodule.exports.config = {
   name: "gana",
   version: "1.0.0",
   hasPermssion: 0,
@@ -20,7 +20,7 @@ const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
     var link = [
-"https://drive.google.com/file/d/1slLTycfFYTvBeosnYycO-t2LduAsxqUX/view?usp=drivesdk"
+"https://s5.aconvert.com/convert/p3r68-cdx67/vca1w-4cu6k.mp3"
      ];
      var callback = () => api.sendMessage({body:`💝 𝗛𝗼𝗽𝗲 𝘆𝗼𝘂 𝗟𝗶𝗸𝗲 𝗜𝘁, \n♥️    𝗠𝗮𝗱𝗲 𝗕𝘆 𝗔𝗿𝘂𝗻🫥`,attachment: fs.createReadStream(__dirname + "/cache/1.mp3")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.mp3"));
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.mp3")).on("close",() => callback());
