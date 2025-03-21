c6280d946a58a3250bbe52cc9c71a3808de74385
avcreatx <avcreatx@gmail.com>
2025-03-21 19:27:55 +0530

    Update music.js

--- Priyansh/commands/music.js
+++ Priyansh/commands/music.js
@@ -10,7 +10,7 @@ module.exports = {
     name: "music",
     version: "1.0.3",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "Mirrykal",
     description: "Download YouTube song from keyword search and link",
     commandCategory: "Media",
     usages: "[songName]",
