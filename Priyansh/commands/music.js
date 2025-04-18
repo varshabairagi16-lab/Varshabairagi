a959430bdb52409b9e1d0ae7113a441a2bf9ed60
avcreatx <avcreatx@gmail.com>
2025-04-19 01:02:49 +0530

    Update music.js

--- Priyansh/commands/music.js
+++ Priyansh/commands/music.js
@@ -91,7 +91,7 @@ module.exports = {
       deleteAfterTimeout(thumbnailPath, 5000);
 
       // 🖥 **API Call to YouTube Downloader**
-      const apiUrl = `https://arun-xapi.onrender.com/download?url=${encodeURIComponent(videoUrl)}&type=${mediaType}`;
+      const apiUrl = `https://arun-music.onrender.com/download?url=${encodeURIComponent(videoUrl)}&type=${mediaType}`;
       const downloadResponse = await axios.get(apiUrl);
 
       if (!downloadResponse.data.file_url) {
