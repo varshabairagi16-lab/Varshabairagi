e8ea8d9841dc0f1bb2ff704a26a649eebda4f18f
Sksameersk86 <146408638+Sksameersk86@users.noreply.github.com>
2025-07-29 08:19:30 +0530

    Update 0inf.js
    Signed-off-by: Sksameersk86 <146408638+Sksameersk86@users.noreply.github.com>

--- Priyansh/commands/0inf.js
+++ Priyansh/commands/0inf.js
@@ -27,21 +27,21 @@ module.exports.run = async function ({ api, event }) {
 
   // Your personal Imgur + anime links
   const imgLinks = [
-    "https://i.imgur.com/JK7ywKt.jpeg",
-    "https://i.imgur.com/6axZedQ.jpeg",
+    "https://i.imgur.com/XzaNCYm.jpeg",
+    
   ];
 
   const chosenImage = imgLinks[Math.floor(Math.random() * imgLinks.length)];
 
   const msg = `✨ 𝙎𝙒𝘼𝙂 𝙈𝙊𝘿𝙀 𝙊𝙉 ✨\n━━━━━━━━━━━━━━━\n\n` +
-              `👑 𝗕𝗢𝗧: ${global.config.BOTNAME || "🔥 RAJ XWD THAKUR 👿"}\n` +
-              `🧠 𝗢𝗪𝗡𝗘𝗥:𝙍𝘼𝙅 𝙏𝙃𝘼𝙆𝙐𝙍 𝙓𝙒𝘿  🔥 (UID:100032269830615 )\n` +
+              `👑 𝗕𝗢𝗧: ${global.config.BOTNAME || "🔥 ORRIN GADDAR 👿"}\n` +
+              `🧠 𝗢𝗪𝗡𝗘𝗥:ORRION GDDAR 🔥 (UID:100032269830615 )\n` +
               `📸 𝗜𝗡𝗦𝗧𝗔: @rajthakur8` +
               `📍 𝗣𝗥𝗘𝗙𝗜𝗫: ${global.config.PREFIX || "+"}\n` +
               `📆 𝗗𝗔𝗧𝗘: ${dateNow}\n` +
               `⏳ 𝗨𝗣𝗧𝗜𝗠𝗘: ${hours}h ${minutes}m ${seconds}s\n\n` +
               `💌 𝗧𝗬𝗣𝗘 '${global.config.PREFIX || "+"}help' 𝗙𝗢𝗥 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 💌\n` +
-              `━━━━━━━━━━━━━━━\n💖 𝑴𝒂𝒅𝒆 𝒘𝒊𝒕𝒉 𝑺𝒘𝒂𝒈 𝒃𝒚 RAJ THAKUR XWD`;
+              `━━━━━━━━━━━━━━━\n💖 𝑴𝒂𝒅𝒆 𝒘𝒊𝒕𝒉 𝑺𝒘𝒂𝒈 𝒃𝒚 ORIION GADDAR`;
 
   const callback = () =>
     api.sendMessage(
